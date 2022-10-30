3332a594793193ecd5462f5fbc808c0e059708ff
afunwa <afunwaemmanuel505@gmail.com>
2022-10-30 11:10:03 +0100

    Update intro.js

--- intro.js
+++ intro.js
@@ -13,3 +13,21 @@ close_button.addEventListener('click', function(){
     close_button.style.display = 'none';
     harmburger_menu.style.display = 'block';
 });
+
+function reveal() {
+    var reveal = document.querySelectorAll(".reveal");
+  
+    for (var i = 0; i < reveal.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveal[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveal[i].classList.add("active");
+      } else {
+        reveal[i].classList.remove("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", reveal);
